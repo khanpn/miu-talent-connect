fcfce2d60fb17f3fc0365193356bb43d811c8c6a
Khanh Nguyen <pkhanhit@gmail.com>
2024-12-04 20:03:29 -0600

    fix(api): enable scheduling

--- backend/src/main/java/edu/miu/cs/mtc/api/MIUTalentConnectApisApplication.java
+++ backend/src/main/java/edu/miu/cs/mtc/api/MIUTalentConnectApisApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableMongoAuditing
 @EnableCaching
+@EnableScheduling
 public class MIUTalentConnectApisApplication {
 
   public static void main(String[] args) {
